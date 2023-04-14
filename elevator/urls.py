af7b4de781c7b05a3b6ceecab5e0d092885bdac2
nikhilkotiya8 <nikhilkotiya8@gmail.com>
2023-04-15 01:31:06 +0530

    testing gcloud prodution ci-cd

--- elevator/urls.py
+++ elevator/urls.py
@@ -18,7 +18,7 @@ from django.urls import path,include
 from drf_spectacular.views import SpectacularAPIView,SpectacularSwaggerView
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('api/schema/', SpectacularAPIView.as_view(),name='schema'),
-    path('api/schema/docs', SpectacularSwaggerView.as_view(url_name='schema')),
+    # path('api/schema/', SpectacularAPIView.as_view(),name='schema'),
+    # path('api/schema/docs', SpectacularSwaggerView.as_view(url_name='schema')),
     path("",include("elevator_app.urls")),
 ]
